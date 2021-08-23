2c3e2d9f9dcd95d46ab6af6a2650a46ab318214b
Carlos Paulino <chgp@riseup.net>
2021-08-23 16:58:41 -0300

    small fix 2, just  close_write and moved_to on inotifywait

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-inotifywait -q --format '%f' -m -r -e close_write,moved_to,create . | xargs -I{} -r sh -c 'curl https://localhost/api/unpack/$(basename {}) -k'
+inotifywait -q --format '%f' -m -r -e close_write,moved_to . | xargs -I{} -r sh -c 'curl https://localhost/api/unpack/$(basename {}) -k'
