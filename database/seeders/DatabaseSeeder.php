45fdec8cdda3d710c6c5ae334608b5a73d14911c
Carlos Paulino <chgp@riseup.net>
2021-10-18 20:20:30 -0300

    cleaning unused in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -68,27 +68,5 @@ class DatabaseSeeder extends Seeder
                 'draft' => false
             ]);
         }
-
-        /*
-                $this->call('UserSeeder');
-
-        Quote::create([
-            'text' => 'Success is going from failure to failure without losing your enthusiasm',
-            'author' => 'Winston Churchill',
-            'background' => '1.jpg'
-        ]);
-
-        Quote::create([
-            'text' => 'Dream big and dare to fail',
-            'author' => 'Norman Vaughan',
-            'background' => '2.jpg'
-        ]);
-
-        Quote::create([
-            'text' => 'It does not matter how slowly you go as long as you do not stop',
-            'author' => 'Confucius',
-            'background' => '3.jpg'
-        ]);
-        */
     }
 }
