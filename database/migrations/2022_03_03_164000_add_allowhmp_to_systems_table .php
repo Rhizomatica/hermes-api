cf38e5dfbdf5e65855d34e1c962ad577de51ac14
Carlos Paulino <chgp@riseup.net>
2022-03-03 16:49:25 -0300

    migration to include allowhmp col on systems table

---
+++ database/migrations/2022_03_03_164000_add_allowhmp_to_systems_table .php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddEmailIdToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('systems', function (Blueprint $table) {
+            //
+            $table->string('allowhmp')->default('admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('systems', function (Blueprint $table) {
+            //
+        });
+    }
+}
+
