75460b0cd364dc90d36f67e35d799051fca1ac43
mtsthibau <mtsthibau@gmail.com>
2022-12-26 10:34:23 +1100

    FEAT | API - Save/updatefrequency schedule

--- app/Http/Controllers/CallerController.php
+++ app/Http/Controllers/CallerController.php
@@ -28,7 +28,7 @@ class CallerController extends Controller
 	 */
 	public function showSched($id)
 	{
-	return response()->json(Caller::find($id));
+		return response()->json(Caller::find($id));
 	}
 
 	/**
@@ -39,27 +39,25 @@ class CallerController extends Controller
 	 */
 	public function createSched(Request $request)
 	{
-	$this->validate($request, [
-		'title' => 'required|unique:caller',
-		'stations' => 'required|array', 
-		'starttime' => 'required|date_format:H:i:s|before:stoptime',
-		'stoptime' => 'required|date_format:H:i:s|after:starttime',
-		'enable' => 'required|boolean'
+		$this->validate($request, [
+			'title' => 'required|unique:caller',
+			'stations' => 'required|array',
+			'starttime' => 'required|date_format:H:i:s|before:stoptime',
+			'stoptime' => 'required|date_format:H:i:s|after:starttime',
+			'enable' => 'required|boolean'
 		]);
 
-	if ( ! $request->title || ! $request->starttime || ! $request->stoptime || $request->enable === null){
-			return response()->json(['message' => 'caller: require all fields ' ], 500);
-	}
+		if (!$request->title || !$request->starttime || !$request->stoptime || $request->enable === null || $request->frequency === null) {
+			return response()->json(['message' => 'caller: require all fields '], 500);
+		}
 
-	$schedule = Caller::create($request->all());
-		if (! $schedule ){
-			return response()->json(['message' => 'caller: cant\'t create a schedule: ' ], 500);
-	}
-		else {
-		//Log::info('creating schedule ' . $schedule);
-			return response()->json( $request->all() , 200);
-	}
-
+		$schedule = Caller::create($request->all());
+		if (!$schedule) {
+			return response()->json(['message' => 'caller: cant\'t create a schedule: '], 500);
+		} else {
+			//Log::info('creating schedule ' . $schedule);
+			return response()->json($request->all(), 200);
+		}
 	}
 
 	/**
@@ -70,20 +68,20 @@ class CallerController extends Controller
 	 */
 	public function updateSched($id, Request $request)
 	{
-	$this->validate($request, [
-		'title' => 'required',
-		'stations' => 'required|array', 
-		'starttime' => 'required|date_format:H:i:s|before:stoptime',
-		'stoptime' => 'required|date_format:H:i:s|after:starttime',
-		'enable' => 'required|boolean'
+		$this->validate($request, [
+			'title' => 'required',
+			'stations' => 'required|array',
+			'starttime' => 'required|date_format:H:i:s|before:stoptime',
+			'stoptime' => 'required|date_format:H:i:s|after:starttime',
+			'enable' => 'required|boolean',
+			'frequency' => 'required'
 		]);
-		if($schedule = Caller::findOrFail($id)){
+		if ($schedule = Caller::findOrFail($id)) {
 			$schedule->update($request->all());
-		Log::info('update schedule' . $id);
+			Log::info('update schedule' . $id);
 			return response()->json($request, 200);
-		}
-		else{
-		Log::warning('schedule cant find to update' . $id);
+		} else {
+			Log::warning('schedule cant find to update' . $id);
 			return response()->json(['message' => 'cant find  schedule' . $id], 404);
 		}
 	}
@@ -95,9 +93,9 @@ class CallerController extends Controller
 	 */
 	public function deleteSched($id)
 	{
-	$schedule = Caller::findOrFail($id);
+		$schedule = Caller::findOrFail($id);
 		Caller::findOrFail($id)->delete();
-	Log::info('delete schedule ' . $id);
+		Log::info('delete schedule ' . $id);
 		return response()->json(['message' => 'Delete sucessfully schedule: ' . $id], 200);
 	}
 
@@ -108,7 +106,7 @@ class CallerController extends Controller
 	 * @return Json
 	 */
 	public function setEnable($id)
-   {
+	{
 		// return response()->json(['unhide' . $id . 'Sucessfully'], 200);
 	}
 }
