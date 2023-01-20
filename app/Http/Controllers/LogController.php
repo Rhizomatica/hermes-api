13ff2e36b499a33536fddbf3711f8b33b5fd0f27
mtsthibau <mtsthibau@gmail.com>
2023-01-20 12:12:46 +1100

    FEAT | API - LOG Controller

---
+++ app/Http/Controllers/LogController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Log;
+use Illuminate\Http\Request;
+
+class LogController extends Controller
+{
+    public function saveLog()
+    {
+        
+    }
+}
---
+++ app/Log.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Log extends Model {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'controller', 'error_code', 'error_message', 'stacktrace', 'user_id', 'updated_at', 'created_at'
+    ];
+}
