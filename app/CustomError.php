c557b4f94da8442849bd34cbcf42e1dd4d7ab176
mtsthibau <mtsthibau@gmail.com>
2023-02-17 12:46:59 +1100

    FIX | API - Remove folder models

---
+++ app/CustomError.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class CustomError extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'controller', 'error_code', 'error_message', 'stacktrace',
+        // 'user_id', 
+        'station', 'updated_at', 'created_at'
+    ];
+}
