639ef17e923af8694ec8cacbc985641b634796e3
Rafael Diniz <rafael@riseup.net>
2023-03-14 18:08:45 +0300

    get wifi settings implemented

--- app/Http/Controllers/WiFiController.php
+++ app/Http/Controllers/WiFiController.php
@@ -8,39 +8,45 @@ class WiFiController extends Controller
 
 	public function getWiFiConfigurations()
 	{
-		$wifiConfigCLI = "";
-		$wifiConfig = explode("\n", exec_cli($wifiConfigCLI))[0];
+		$hostap_file = file_get_contents("/etc/hostapd/hostapd.conf", false);
 
-		if ($wifiConfig != '0') {
-			return response()->json(['message' => 'Server error'], 500);
+		$parsed_cfg = explode("\n", $hostap_file);
+
+		foreach ($parsed_cfg as $i)
+		{
+			if (strpos($i, "=") !== false)
+			{
+				list($name,$value) = explode("=", $i, 2);
+
+				if( $name == "channel" || $name == "ssid" || $name == "wpa_passphrase" )
+					$wifi_settings[$name] = $value;
+			}
 		}
-		
-		return response()->json($wifiConfig, 200);
+
+		if (isset($wifi_settings['channel']) && isset($wifi_settings['ssid']) && isset($wifi_settings['wpa_passphrase']))
+			return response()->json($wifi_settings, 200);
+		else
+			return response()->json(['message' => 'Server error'], 500);}
 	}
 
 	public function saveWiFiConfigurations(Request $request)
 	{
 		$this->validate($request, [
+			'channel' => 'required|string',
 			'ssid' => 'required|string',
-			'password' =>  'required|string',
-			'chanel' => 'required|integer'
+			'wpa_passphrase' =>  'required|string'
 		]);
 
-		$saveWifiConfigCLI = '' . $request->ssid;
-		
-		$wifiConfig = explode("\n", exec_cli($saveWifiConfigCLI))[0];
+        // copy("/etc/hostapd/hostapd.conf.head", "/etc/hostapd/hostapd.conf.head")
+        // exec()
 
-		if ($wifiConfig != '0') {
+        // exec('sudo systemctl restart hostapd', $output, $return_value);
+
+
+		//if ($return_value != '0') {
 			return response()->json(['message' => 'Server error'], 500);
-		}
+		//}
 
-		$restartWifi = '';
-		$applyChanges = explode("\n", exec_cli($restartWifi))[0];
-
-		if ($applyChanges != '0') {
-			return response()->json(['message' => 'Server error'], 500);
-		}
-
-		return response()->json($saveWifiConfigCLI, 200); 
+        //	return response()->json($saveWifiConfigCLI, 200);
 	}
 }
