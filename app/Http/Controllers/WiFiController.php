e75824fef6a2c33718c701840adaef828b8dd34a
mtsthibau <mtsthibau@gmail.com>
2023-03-15 22:14:50 +1100

    FIX | API - Request parameters WiFi Manager

--- app/Http/Controllers/WiFiController.php
+++ app/Http/Controllers/WiFiController.php
@@ -39,9 +39,9 @@ class WiFiController extends Controller
 		if (exec_cli_no("sudo cp /etc/hostapd/hostapd.conf.head /etc/hostapd/hostapd.conf") == false)
 			return response()->json(['message' => 'Server error'], 500);
 
-		exec_cli("sudo sh -c \"echo channel={$channel} >> /etc/hostapd/hostapd.conf\"");
-		exec_cli("sudo sh -c \"echo ssid={$ssid} >> /etc/hostapd/hostapd.conf\"");
-		exec_cli("sudo sh -c \"echo wpa_passphrase={$wpa_passphrase} >> /etc/hostapd/hostapd.conf\"");
+		exec_cli("sudo sh -c \"echo channel={$request->channel} >> /etc/hostapd/hostapd.conf\"");
+		exec_cli("sudo sh -c \"echo ssid={$request->ssid} >> /etc/hostapd/hostapd.conf\"");
+		exec_cli("sudo sh -c \"echo wpa_passphrase={$request->wpa_passphrase} >> /etc/hostapd/hostapd.conf\"");
 
 		if (exec_cli_no('sudo systemctl restart hostapd') == false)
 			return response()->json(['message' => 'Server error'], 500);
