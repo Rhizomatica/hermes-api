f299236d2a10aa576fd379415817c804e18c5752
mtsthibau <mtsthibau@gmail.com>
2023-08-01 19:03:41 +1000

    FIX | API - save error API new request method

--- app/Http/Controllers/ErrorController.php
+++ app/Http/Controllers/ErrorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 use App\CustomError;
 
 class ErrorController extends Controller
@@ -33,6 +34,18 @@ class ErrorController extends Controller
 		$customError->save();
 
 		Log::error($error_message);
+	}
+
+	public function saveErrorApi(Request $request)
+	{
+		$customError = new CustomError;
+		// $log->user_id = 0;
+		$customError->controller = $request->controller;
+		$customError->error_code = $request->error_code;
+		$customError->error_message = $request->error_message;
+		$customError->stacktrace = $request->stacktrace;
+		$customError->station = env('HERMES_NAME');
+		$customError->save();
 	}
 
 	public function deleteCustomError($id = null)
--- routes/web.php
+++ routes/web.php
@@ -103,7 +103,7 @@ $router->group(['prefix' => '/customerrors'], function () use ($router) {
     $router->get('',  ['uses' => 'ErrorController@getCustomErrors']);
     $router->delete('{id}', ['uses' => 'ErrorController@deleteCustomError']);
     $router->delete('', ['uses' => 'ErrorController@deleteCustomError']);
-    $router->post('', ['uses' => 'ErrorController@saveError']);
+    $router->post('', ['uses' => 'ErrorController@saveErrorApi']);
 });
 
 $router->group(['prefix' => '/wifi'], function () use ($router) {
