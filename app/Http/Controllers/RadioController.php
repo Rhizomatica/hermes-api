0efb41f1e62bfe2ebabaa3962accbb9c3e1509ec
mtsthibau <mtsthibau@gmail.com>
2023-08-23 15:23:12 +1000

    FIX | API - Change step validation

--- app/Http/Controllers/RadioController.php
+++ app/Http/Controllers/RadioController.php
@@ -595,12 +595,11 @@ class RadioController extends Controller
 	public function updateStep(Request $request)
 	{
 
-		if ($request['step'] == null) {
-			(new ErrorController)->saveError(get_class($this), 500, 'API Error: Missing step value');
-			return response()->json(['message' => 'Server error'], 500);
-		}
+		$this->validate($request, [
+			'step' => 'required|number'
+		]);
 
-		$output = explode("\n", exec_uc("sbitx_client -c set_freqstep -a " . $request['step']))[0];
+		$output = explode("\n", exec_uc("sbitx_client -c set_freqstep -a " . $request->step))[0];
 
 		if ($output == "OK") {
 			return response(true, 200);
