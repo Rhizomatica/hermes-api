cb7da565d8f5ec87d7a7f826bd9523239041866f
mtsthibau <mtsthibau@gmail.com>
2024-08-14 10:29:01 -0300

    FIX | API - SNR & Bitrate shadowed route 2

--- routes/web.php
+++ routes/web.php
@@ -93,12 +93,13 @@ $router->group(['prefix' => '/radio'], function () use ($router) {
     $router->post('volume/{volume}',  ['uses' => 'RadioController@changeVolume']);
     $router->get('erasesdcard',  ['uses' => 'RadioController@eraseSDCard']);
     $router->post('profile/{profile}',  ['uses' => 'RadioController@setRadioProfile']);
-    $router->get('{profile}',  ['uses' => 'RadioController@getRadioStatus']);
     $router->post('/voice/timeout',  ['uses' => 'RadioController@restartVoiceTimeout']);
     $router->get('/voice/timeout/config',  ['uses' => 'RadioController@getTimeoutConfig']);
     $router->post('/voice/timeout/config/{seconds}',  ['uses' => 'RadioController@setTimeoutConfig']);
     $router->get('/bitrate',  ['uses' => 'RadioController@getBitrate']);
     $router->get('/snr',  ['uses' => 'RadioController@getSNR']);
+
+    $router->get('{profile}',  ['uses' => 'RadioController@getRadioStatus']);   
 });
 
 $router->group(['prefix' => '/geolocation'], function () use ($router) {
