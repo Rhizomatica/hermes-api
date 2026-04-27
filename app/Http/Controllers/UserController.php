5b5d2d11724ba62ccac26d494a593666d79034e3
Matheus Thibau <mtsthibau@gmail.com>
2026-04-27 13:14:07 -0300

    Fix: Correct user retrieval by ID #20

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
 
 	public function showOneUser($id)
 	{
-		$user = User::find('id', $id);
+		$user = User::find($id);
 		
 		if (!$user) {
 			(new ErrorController)->saveError(static::class, 404, 'Could not find user');
